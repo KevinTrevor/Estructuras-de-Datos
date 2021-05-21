a2f2013d03d77036c2d2e64e4625524cfedc2469
yara <yara@192.168.56.1>
2021-05-21 16:46:43 -0400

    Estructuras de Datos - Kevin Rojas 29.582.382
    1. Inicio del algoritmo de Floyd.
    
    21/05/2021

--- src/main/java/GrafoMatriz/TodoCaminoMinimo.java
+++ src/main/java/GrafoMatriz/TodoCaminoMinimo.java
@@ -2,5 +2,15 @@ package GrafoMatriz;
 
 // Algoritmo de Floyd
 public class TodoCaminoMinimo {
+    static final int INFINITO = 100000000;
     
+    public GrafoMatriz grafo;
+    public int[][] pesos;
+    public int[][] distancia;
+    public int[][] camino;
+    public int numVerts;
+    
+    public TodoCaminoMinimo(){
+    
+    }
 }
